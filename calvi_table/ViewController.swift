b9d3eb157e60e7628327093acd4eb28c741f1b5c
Berndhura <wichura@gmx.de>
2017-08-16 14:13:29 +0200

    Initial Commit

---
+++ calvi_table/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  calvi_table
+//
+//  Created by bernd wichura on 16.08.17.
+//  Copyright © 2017 bernd wichura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ calvi_tableTests/calvi_tableTests.swift
@@ -0,0 +1,36 @@
+//
+//  calvi_tableTests.swift
+//  calvi_tableTests
+//
+//  Created by bernd wichura on 16.08.17.
+//  Copyright © 2017 bernd wichura. All rights reserved.
+//
+
+import XCTest
+@testable import calvi_table
+
+class calvi_tableTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
