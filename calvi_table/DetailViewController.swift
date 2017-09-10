55c7fd81faea74851b332b2ad18a2c39873c1563
Berndhura <wichura@gmx.de>
2017-09-10 21:51:03 +0200

    buttons in open ad, layout only

--- calvi_table/DetailViewController.swift
+++ calvi_table/DetailViewController.swift
@@ -94,13 +94,13 @@ class DetailViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDele
         
         if (error == nil) {
             // Perform any operations on signed in user here.
-            let userId = user.userID                  // For client-side use only!
-            let idToken = user.authentication.idToken // Safe to send to the server
-            let fullName = user.profile.name
-            let givenName = user.profile.givenName
-            let familyName = user.profile.familyName
-            let email = user.profile.email
-            // ...
+            //let userId = user.userID                  // For client-side use only!
+            //let idToken = user.authentication.idToken // Safe to send to the server
+            //let fullName = user.profile.name
+            //let givenName = user.profile.givenName
+            //let familyName = user.profile.familyName
+            //let email = user.profile.email
+            print(user.profile.imageURL(withDimension: 400))
         } else {
             print("\(error.localizedDescription)")
         }
