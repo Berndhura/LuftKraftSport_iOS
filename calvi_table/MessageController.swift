8b81db99ef940abad88d994e3ed1d4d9ee38eb44
bernd wichura <conan@bernds-Mac.local>
2017-09-22 12:55:45 +0200

    messages, picture

--- calvi_table/MessageController.swift
+++ calvi_table/MessageController.swift
@@ -18,21 +18,7 @@ class MessagesController: UIViewController {
         super.viewDidLoad()
         
         fetchMessages()
-        
-        //dummydata
-        
-        /*let msg1 = Message(name: "Maul1")
-        let msg2 = Message(name: "Maul2")
-        let msg3 = Message(name: "Maul3")
-        let msg4 = Message(name: "Maul4")
-        let msg5 = Message(name: "Maul5")
-        
-        messages.append(msg1)
-        messages.append(msg2)
-        messages.append(msg3)
-        messages.append(msg4)
-        messages.append(msg5)*/
-        
+
         //table
         self.tableView.contentInset = UIEdgeInsets(top: -64.0, left: 0.0, bottom: 0.0, right: 0.0)
         self.tableView.dataSource = self
@@ -57,7 +43,7 @@ class MessagesController: UIViewController {
         
         let url = URL(string: "http://178.254.54.25:9876/api/V3/messages/forUser?token=\(userToken)")
         
-        print(url)
+        //print(url)
         
         URLSession.shared.dataTask(with: url!) { data, response, error in
             
@@ -118,15 +104,11 @@ extension MessagesController: UITableViewDataSource {
         
         let currentMessage: Message = messages[indexPath.row]
         
-        //title
+        //name
         cell?.name?.text = currentMessage.name
-        
-        /*
-        //description
-        cell?.desc?.text = currentAd.desc
-        
+    
         //image
-        let imageId = getPictureUrl(str: ads[indexPath.item].urls)
+        let imageId = messages[indexPath.item].url
         
         let url = URL(string: "http://178.254.54.25:9876/api/V3/pictures/\(imageId)/thumbnail")
         
@@ -142,7 +124,6 @@ extension MessagesController: UITableViewDataSource {
                 cell?.bild.image = image
             })
             }.resume()
- */
         
         return cell!
     }
