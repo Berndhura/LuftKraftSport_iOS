e720136b7775cf81d1916d7cf106677c625f4dda
Berndhura <wichura@gmx.de>
2018-09-25 22:42:31 +0200

    add home VC, impressum first shot

---
+++ calvi_table/ImpressumViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ImpressumViewController.swift
+//  calvi_table
+//
+//  Created by bernd wichura on 25.09.18.
+//  Copyright © 2018 bernd wichura. All rights reserved.
+//
+
+import Foundation
+
+class ImpressumViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
