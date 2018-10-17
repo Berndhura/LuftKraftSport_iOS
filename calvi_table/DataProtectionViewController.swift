7ede9886403355b8f51b1b44018c4c40435f853c
Berndhura <wichura@gmx.de>
2018-10-17 21:21:24 +0200

    datenschutzerkläruung angepasst

---
+++ calvi_table/DataProtectionViewController.swift
@@ -0,0 +1,24 @@
+//
+//  DataProtectionViewController.swift
+//  calvi_table
+//
+//  Created by bernd wichura on 17.10.18.
+//  Copyright © 2018 bernd wichura. All rights reserved.
+//
+
+import Foundation
+
+class DataProtectionViewController: UIViewController {
+    
+    
+    @IBOutlet weak var dataProtectionContent: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+       // dataProtectionContent.text
+    }
+    
+}
+
+
