1ddf9780fb438d010775fffab947e40b8d6d975f
Berndhura <wichura@gmx.de>
2018-10-17 21:29:40 +0200

    color for empty tableView

--- calvi_table/UITableView+setEmptyMessage.swift
+++ calvi_table/UITableView+setEmptyMessage.swift
@@ -17,14 +17,15 @@ extension UITableView {
     func setEmptyMessage(_ message: String) {
         let messageLabel = UILabel(frame: CGRect(x: 0, y: 0, width: self.bounds.size.width, height: self.bounds.size.height))
         messageLabel.text = message
-        messageLabel.textColor = .black
-        messageLabel.numberOfLines = 0;
-        messageLabel.textAlignment = .center;
+        messageLabel.textColor = appMainColorBlue
+        messageLabel.numberOfLines = 0
+        messageLabel.textAlignment = .center
         messageLabel.font = UIFont(name: "Verdana", size: 15)
         messageLabel.sizeToFit()
         
-        self.backgroundView = messageLabel;
-        self.separatorStyle = .none;
+        self.backgroundView = messageLabel
+        self.backgroundColor = .white
+        self.separatorStyle = .none
     }
     
     func restore() {
